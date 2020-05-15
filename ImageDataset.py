a8a491c512e09a7e6ebde18605052d1f1e7edbfd
hhhkkkjjj <hekaijun1017@gmail.com>
2020-05-15 00:53:33 -0700

    test

--- ImageDataset.py
+++ ImageDataset.py
@@ -4,6 +4,18 @@ import numpy as np
 import torch
 from ImgProcess import car_center
 
+IMG_WIDTH = 1024
+IMG_HEIGHT = IMG_WIDTH // 16 * 5
+
+
+def preprocess_image(img):
+    img = img[img.shape[0] // 2:]
+    bg = np.ones_like(img) * img.mean(1, keepdims=True).astype(img.dtype)
+    bg = bg[:, :img.shape[1] // 6]
+    img = np.concatenate([bg, img, bg], 1)
+    img = cv2.resize(img, (IMG_WIDTH, IMG_HEIGHT))
+    return (img / 255).astype('float32')
+
 
 class ImageDataset(Dataset):
     def __init__(self, data, root, camera):
@@ -22,6 +34,7 @@ class ImageDataset(Dataset):
         img_id, labels = self.data.to_numpy()[idx]
         img_name = self.root + img_id + '.jpg'
         img = cv2.imread(img_name)
+        img = preprocess_image(img)
         img = np.rollaxis(img, 2, 0)
         center, center_far = car_center(img, labels, self.camera)
         center_far = np.rollaxis(center_far, 2, 0)
